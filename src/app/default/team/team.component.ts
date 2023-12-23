6ef5865431daf75b77dec946ce8fc26da018b9a4
TejasMurtadak <149615563+TejasMurtadak@users.noreply.github.com>
2023-12-23 12:58:46 +0530

    Updated about us

--- src/app/default/team/team.component.ts
+++ src/app/default/team/team.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrl: './team.component.scss',
 })
-export class TeamComponent {}
+export class TeamComponent implements OnInit{
+  ngOnInit(): void {
+    
+    this.scrollToTop();
+  }
+  scrollToTop() {
+    window.scroll({ top: 0, behavior: 'smooth' });
+  }
+}
